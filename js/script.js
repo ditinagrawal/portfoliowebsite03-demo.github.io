f61586ce7ebc845e6ac5531110d7b48dc5f9c4e9
DitinAgrawal <ditin.agrawal05@gmail.com>
2022-10-23 16:45:47 +0530

    Portfolio Website

---
+++ js/script.js
@@ -0,0 +1,72 @@
+
+
+/*=========================
+Toggle Header 
+==========================*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", ()=>{
+	toggleNavbar();
+	document.body.classList.toggle("hide-scrolling");
+});
+
+function toggleNavbar(){
+	document.querySelector(".header").classList.toggle("active");
+}
+
+
+
+
+
+/*=========================
+About Tabs
+==========================*/
+const tabsContainer = document.querySelector(".about-tabs"),
+	  aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click",(e) =>{
+	if(e.target.classList.contains("tab-item") && !e.target.classList.contains("active")){
+		tabsContainer.querySelector(".active").classList.remove("active");
+		e.target.classList.add("active");
+		const target = e.target.getAttribute("data-target");
+		aboutSection.querySelector(".tab-content.active").classList.remove("active");
+		aboutSection.querySelector(target).classList.add("active");
+	}
+});
+
+
+
+/*=========================
+Portfolio Popup
+==========================*/
+document.addEventListener("click", (e)=>{
+	if (e.target.classList.contains("view-project-btn")){
+		togglePortfolioPopup();
+		document.querySelector(".portfolio-popup").scrollTo(0,0);
+		portfolioItemDetails(e.target.parentElement);
+	}
+});
+
+function togglePortfolioPopup(){
+	document.querySelector(".portfolio-popup").classList.toggle("open");
+	document.body.classList.toggle("hide-scrolling");
+	document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+	document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+	document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+	document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+
+}
+
+// ======= hide popup when clicked outside ========
+document.addEventListener("click", (e)=>{
+	if (e.target.classList.contains("pp-inner")){
+		togglePortfolioPopup();
+	}
+})
+
